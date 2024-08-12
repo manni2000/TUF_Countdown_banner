6d3f1b7559817bd540ddbb47f6bbd7a0a1742c30
Manish kumar <manishmandal9734@gmail.com>
2024-08-12 21:11:56 +0530

    update db

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'newdb',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
