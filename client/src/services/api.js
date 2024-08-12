4273aef88c1e3553b8225d407d25d53211610776
Manish kumar <manishmandal9734@gmail.com>
2024-08-12 20:53:46 +0530

    commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'https://tuf-countdown-banner-api.onrender.com/api';
+// const API_URL = 'https://tuf-countdown-banner-api.onrender.com/api';
+
+export const API_URL = process.env.NODE_ENV === 'development' ?
+    `http://localhost:5000/api` :
+   'https://tuf-countdown-banner-api.onrender.com/api';
 
 export const getBanner = async () => {
   const response = await axios.get(`${API_URL}/banner`);
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
+require('./config/db');
 
 const bannerRoutes = require('./routes/bannerRoutes');
 
@@ -11,15 +12,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
 app.use('/api', bannerRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
