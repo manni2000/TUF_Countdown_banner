f43438912a348ef9771e6becba7a4c5c36857a0a
Manish kumar <manishmandal9734@gmail.com>
2024-08-12 20:20:15 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -10,9 +10,15 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use('/api', bannerRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
